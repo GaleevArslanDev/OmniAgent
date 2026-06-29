0d2f861f963c8799380cf2c86671aa8bb9e2fc89
GaleevArslanDev <galeev_arslan2021@bk.ru>
2026-06-29 19:53:46 +0300

    Add look at nearest block tool

---
+++ tools/minecraft/look_at_nearest.py
@@ -0,0 +1,32 @@
+﻿from typing import Any
+
+from clients.minecraft.client import MinecraftClient
+from tool import Tool
+
+
+class LookAtNearestTool(Tool):
+    name = "look_at_nearest"
+    description = "Посмотреть на ближайший блок нужного типа."
+    args_schema = {
+        "block_name": "str - тип блока. Например, oak_log",
+    }
+
+    def use(self, client: MinecraftClient, arguments: dict) -> tuple[bool, dict[str, Any]]:
+        target_name = arguments["block_name"]
+        blocks = client.observe()["vision"]["nearby_objects"]
+        nearest_block = next((b for b in blocks if b["name"] == target_name), None)
+
+        if not nearest_block:
+            return False, {"error": "no_such_block_in_nearest_objects"}
+
+        x, y, z = nearest_block["x"], nearest_block["y"], nearest_block["z"]
+
+        before = client.observe()["vision"]["block_at_cursor"]
+        success = client.look_at_coords(x, y, z)
+        after = client.observe()["vision"]["block_at_cursor"]
+
+        return success, {
+            "looked_at": {"x": x, "y": y, "z": z},
+            "block_at_cursor_before": before,
+            "block_at_cursor_after": after,
+        }
